bfcf05c1978864ee820ddad55654589d5443ff7f
nezca <oldyth@gmail.com>
2017-05-29 19:18:45 +0900

    deploy for first

---
+++ index.js
@@ -0,0 +1,81 @@
+//------------ npm express setting --------------
+var express = require('express');
+var app = express();
+var multer = require('multer');
+app.use(express.static('public'));
+var bootstrap = require("express-bootstrap-service");
+var bodyPaser = require('body-parser');
+
+//------------ local server port setting ---------
+app.set('port', (process.env.PORT || 5000));
+
+//----------- pug(former 'jade') setting -----------
+app.set('view engine','pug');
+app.set('views', './views');
+app.use(bootstrap.serve);
+app.use(bodyPaser.urlencoded({extended:false}));
+
+////--------- ClearDB(mysql) Setting -------------------
+//var mysql      = require('mysql');
+//var db_config = {
+//  host     : 'us-cdbr-iron-east-03.cleardb.net', 
+//  user     : 'b52b1fabc5d89f',
+//  password : '3fda8c47',
+//  database : 'heroku_e54ff6534eba235'
+//};
+//var connection;
+//
+//function handleDisconnect() {
+//  connection = mysql.createConnection(db_config);
+//  connection.connect(function(err) {              
+//    if(err) {
+//      console.log('error when connecting to db:', err);
+//      setTimeout(handleDisconnect, 2000);
+//    }                                     
+//  });                                     
+//                                          
+//  connection.on('error', function(err) {
+//    console.log('db error', err);
+//    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//      handleDisconnect();                         
+//    } else {
+//      throw err;                                  
+//    }
+//  });
+//}
+//
+//
+//handleDisconnect();
+
+//------------ Pages Routing -------------------------
+  //------------ main(search) page -------------------------
+app.get('/', function(req,res){
+  res.render('intro');
+});
+
+  //------------ sub(about) page -------------------------
+app.get('/about_us', function(req,res){
+  res.render('about_us');
+});
+//  //------------ 결과 페이지 난이도 조건만 넣음 ---------------
+//
+//app.post('/return', function(req,res){  
+//  var province_code = req.body.province_code;
+//  var duration_rate_code = req.body.duration_rate_code;
+//  var difficulty_type_code = req.body.difficulty_type_code;  
+//  var budget = req.body.budget;
+//  var route_type_code = req.body.route_type_code;
+//  var sql = 'SELECT * FROM course_table WHERE province_code = ? and duration_rate_code = ? and difficulty_type_code <= ? and budget <= ? and route_type_code = ? limit 4';
+//  connection.query(sql, [province_code, duration_rate_code, difficulty_type_code, budget, route_type_code], function(err, gilhub, fields){
+//    if(err){
+//      console.log(err);
+//      res.status(500).send('what the hell!');
+//      }
+//    res.render('return',{gilhub:gilhub});   
+//  });
+//});
+
+//----- node.js tutorial's app.listen method -----
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
